757fd9392b78970dab35aa807b1ebeaad38b97dc
Yuancheng Zhang <en@endaye.com>
2018-11-17 05:50:39 +0800

    can upload file and save to server disk

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var uploadRouter = require('./routes/upload');
 
 var app = express();
 
@@ -14,7 +15,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/upload', uploadRouter);
+
+
 
 module.exports = app;
---
+++ routes/upload.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer().single('zip');
+
+/* POST upload API. */
+router.post('/zip', (req, res, next) => {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+      console.log(err);
+      res.send('You are a bad boy! ' + err);
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      console.log(err);
+      res.send('You are a really bad boy! ' + err);
+    }
+    console.log(req.file);
+    if (req.file) {
+      fs.writeFile('uploads/text.jpg', req.file.buffer, (err) => {
+        if (err) {
+          res.send('You are a bad bad boy! ' + err);
+        }
+        console.log('The file has been saved!');
+      });
+    } else {
+      res.send('Leave me alone, lazy boy!');
+    }
+
+    res.send('You are a kool boy!');
+  })
+});
+
+module.exports = router;
