82cb90a93dc3e4f088c8fa71e791687e4c60db68
Yuancheng Zhang <old.far.hot@gmail.com>
2018-12-18 20:37:04 +0800

    beautify UI

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,5 @@
 $(() => {
+    $("#upload-result").hide();
     $('#upload-form').submit((e) => {
         event.preventDefault();
 
@@ -15,15 +16,24 @@ $(() => {
             data: formData,
             processData: false, // tell jQuery not to process the data
             contentType: false, // tell jQuery not to set contentType
-            success: (data) => {
+            success: function (data) {
                 $("#upload-result").text(data);
                 $("#upload-result").css("color", "green");
+                $("#upload-result").show();
             },
-            error: (e) => {
+            error: function (e) {
                 console.log(e);
                 $("#upload-result").text(JSON.stringify(e));
                 $("#upload-result").css("color", "red");
+                $("#upload-result").show();
             }
         });
     });
+
+    $('#input-group-file-01').on('change', function () {
+        //get the file name
+        var fileName = $(this).val();
+        //replace the "Choose a file" label
+        $(this).next('.custom-file-label').text(fileName);
+    })
 });
